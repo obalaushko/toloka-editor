970d875f8b901b0053da77e3ddc1619fbae13390
Deimos <alex279368@gmail.com>
2024-09-13 15:11:46 +0300

    Add folder aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	server: {
+		https: {
+			key: '', // Provide a valid key here
+		},
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@styles': path.resolve(__dirname, './src/styles'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@store': path.resolve(__dirname, './src/store'),
+			'@templates': path.resolve(__dirname, './src/templates'),
+			'@types': path.resolve(__dirname, './src/types'),
+			'@utils': path.resolve(__dirname, './src/utils'),
+			'@hooks': path.resolve(__dirname, './src/hooks'),
+			'@constants': path.resolve(__dirname, './src/constants'),
+			'@api': path.resolve(__dirname, './src/api'),
+			'@router': path.resolve(__dirname, './src/router'),
+		},
+	},
+	plugins: [react(), basicSsl()],
 })
